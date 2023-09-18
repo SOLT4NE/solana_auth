43e620bcc3b82b08f1b98da1e51cd8df110d4e68
SOLT4NE <alicasaalicasa8@gmail.com>
2023-09-19 00:20:41 +0100

    first commit1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import "~/styles/globals.css";
 import React, { useMemo } from "react";
 import { ConnectionProvider, WalletProvider, } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { LedgerWalletAdapter, PhantomWalletAdapter, SolflareWalletAdapter,TorusWalletAdapter,   } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 import type { AppProps } from "next/app";
@@ -25,12 +25,16 @@ const MyApp: AppType<{ session: Session | null }> = ({
   const wallets = useMemo(
     () => [
       new PhantomWalletAdapter(),
+            new SolflareWalletAdapter({ network }),
+            //new TorusWalletAdapter(),
+           // new SolletExtensionWalletAdapter(),
+         //  new LedgerWalletAdapter()
     ],
     []
   );
   return (
    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
+      <WalletProvider wallets={wallets} >
         <WalletModalProvider>
           <SessionProvider session={session} refetchInterval={0}>
             <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import { getCsrfToken, signIn, signOut, useSession } from "next-auth/react";
-
 import { useWalletModal } from "@solana/wallet-adapter-react-ui";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { SigninMessage } from "../utils/SigninMessage";
 import bs58 from "bs58";
-import { useEffect } from "react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useEffect, useState } from "react";
+import AuthenticatedContent from "../components/protected"; // Import your AuthenticatedContent component
 
 export default function Header() {
   const { data: session, status } = useSession();
@@ -13,13 +14,12 @@ export default function Header() {
 
   const wallet = useWallet();
   const walletModal = useWalletModal();
+  const [showSignInModal, setShowSignInModal] = useState(false);
+  const [signInError, setSignInError] = useState(null); // State to track sign-in errors
+  const [authenticated, setAuthenticated] = useState(false); // State to track authentication status
 
   const handleSignIn = async () => {
     try {
-      if (!wallet.connected) {
-        walletModal.setVisible(true);
-      }
-
       const csrf = await getCsrfToken();
       if (!wallet.publicKey || !csrf || !wallet.signMessage) return;
 
@@ -34,87 +34,114 @@ export default function Header() {
       const signature = await wallet.signMessage(data);
       const serializedSignature = bs58.encode(signature);
 
-      signIn("credentials", {
+      const result = await signIn("credentials", {
         message: JSON.stringify(message),
         redirect: false,
         signature: serializedSignature,
       });
+
+      if (result?.error) {
+        setSignInError(result.error);
+      } else {
+        setAuthenticated(true); // Set authenticated to true when sign-in is successful
+        setShowSignInModal(false);
+      }
     } catch (error) {
       console.log(error);
+      setSignInError("Error signing in. Please try again.");
     }
   };
 
   useEffect(() => {
     if (wallet.connected && status === "unauthenticated") {
-      handleSignIn();
+      setShowSignInModal(true);
     }
   }, [wallet.connected]);
 
   return (
-<header className="bg-blue-500 p-4">
-  <noscript>
-    <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
-  </noscript>
-  <div className="container mx-auto">
-    <p className="text-white">
-      {!session && (
-        <>
-          <span>You are not signed in</span>
-          <span
-            onClick={handleSignIn}
-            className="ml-2 px-4 py-2 bg-blue-700 text-white rounded-lg cursor-pointer"
-          >
-            Sign in
-          </span>
-        </>
-      )}
-      {session?.user && (
-        <>
-          {session.user.image && (
-            <span
-              className="ml-4 px-4 py-4 w-[200px] rounded-full bg-white"
-              style={{ backgroundImage: `url('${session.user.image}')` }}
-            />
+    <header className="bg-blue-500 p-4">
+      <noscript>
+        <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
+      </noscript>
+      <div className="container mx-auto">
+        <p className="text-white">
+          {!session && (
+            <>
+              <span>You are not signed in</span>
+              <WalletMultiButton />
+            </>
           )}
-          <span className="text-white">
-            <small>Signed in as</small>
-            <br />
-            <strong>{session.user.email ?? session.user.name}</strong>
-          </span>
-          <a
-            href={`/api/auth/signout`}
-            className="ml-2 text-white hover:underline cursor-pointer"
-            onClick={(e) => {
-              e.preventDefault();
-              signOut();
-            }}
-          >
-            Sign out
-          </a>
-        </>
-      )}
-    </p>
-  </div>
-  <nav className="container mx-auto mt-4">
-    <ul className="flex space-x-4">
-      <li>
-        <Link legacyBehavior href="/">
-          <a className="text-white hover:underline">Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link legacyBehavior href="/api/examples/protected">
-          <a className="text-white hover:underline">Protected API Route</a>
-        </Link>
-      </li>
-      <li>
-        <Link legacyBehavior href="/me">
-          <a className="text-white hover:underline">Me</a>
-        </Link>
-      </li>
-    </ul>
-  </nav>
-</header>
+          {session?.user && authenticated && (
+            <>
+              {session.user.image && (
+                <span
+                  className="ml-4 px-4 py-4 w-[200px] rounded-full bg-white"
+                  style={{ backgroundImage: `url('${session.user.image}')` }}
+                />
+              )}
+              <span className="text-white">
+                <small>Signed in as</small>
+                <br />
+                <strong>{session.user.email ?? session.user.name}</strong>
+              </span>
+              <a
+                href={`/api/auth/signout`}
+                className="ml-2 text-white hover:underline cursor-pointer"
+                onClick={(e) => {
+                  e.preventDefault();
+                  signOut();
+                }}
+              >
+                Sign out
+              </a>
+            </>
+          )}
+        </p>
+      </div>
+      <nav className="container mx-auto mt-4">
+        <ul className="flex space-x-4">
+          <li>
+            <Link legacyBehavior href="/">
+              <a className="text-white hover:underline">Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link legacyBehavior href="/api/examples/protected">
+              <a className="text-white hover:underline">Protected API Route</a>
+            </Link>
+          </li>
+          <li>
+            <Link legacyBehavior href="/me">
+              <a className="text-white hover:underline">Me</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
+      {/* Modal for Sign In */}
+      {showSignInModal && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-white p-4 shadow-lg rounded-lg">
+            {signInError && (
+              <p className="text-red-500">{signInError}</p>
+            )}
+            <p className="text-center">
+              You need to sign in with your Solana Wallet to access this content.
+            </p>
+            <button
+              onClick={() => {
+                handleSignIn();
+              }}
+              className="mt-4 px-4 py-2 bg-blue-700 text-white rounded-lg cursor-pointer"
+            >
+              Sign in with Solana Wallet
+            </button>
+          </div>
+        </div>
+      )}
+
+      {/* Authenticated Content */}
+      {session?.user && authenticated && <AuthenticatedContent session={session} />}
+    </header>
   );
 }
